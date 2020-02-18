5484be8f41e80dc24beb3a3360487e0d9ea855bd
Андрей Боев <vigri.i@mail.ru>
2020-02-18 16:51:38 +0300

    Сделана форма регистрации бегуна

---
+++ MarathonApp/MarathonApp/Pages/RunnerPage/PageRegEvent.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MarathonApp.Pages.RunnerPage
+{
+    /// <summary>
+    /// Interaction logic for PageRegEvent.xaml
+    /// </summary>
+    public partial class PageRegEvent : Page
+    {
+        public PageRegEvent()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- MarathonApp/MarathonApp/Pages/RunnerPage/PageRegRunner.xaml.cs
+++ MarathonApp/MarathonApp/Pages/RunnerPage/PageRegRunner.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -15,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace MarathonApp.Pages.RunnerPage
 {
     /// <summary>
@@ -22,7 +24,7 @@ namespace MarathonApp.Pages.RunnerPage
     /// </summary>
     public partial class PageRegRunner : Page
     {
-        private string _filePath = ""; 
+        byte[] _imageData = null;
         public PageRegRunner()
         {
             InitializeComponent();
@@ -41,68 +43,93 @@ namespace MarathonApp.Pages.RunnerPage
         {
             StringBuilder error = new StringBuilder();
             //erorr.Clear();
-            if (TBEmail.Text == "" || TBPassword.Password == "" || TBPasswordCheck.Password == "" || TBFirstName.Text == "" || TBLastName.Text == "")
+            if (TBEmail.Text == "" || TBPassword.Password == "" || TBPasswordCheck.Password == "" || TBFirstName.Text == "" || TBLastName.Text == "" || ImageProfile == null)
             {
                 error.AppendLine("Все поля обязательны для заполнения!");
             }
+            else
+            {
+                //bool hasDigits = false;
+                //bool hasUpper = false;
+                //bool hasLower = false;
 
-            //bool hasDigits = false;
-            //bool hasUpper = false;
-            //bool hasLower = false;
-
-            bool hasDigits = false, hasUpper = false, hasLower = false;
+                bool hasDigits = false, hasUpper = false, hasLower = false;
 
 
-            foreach (var symb in TBPassword.Password)
-            {
-                if (char.IsDigit(symb))
+                foreach (var symb in TBPassword.Password)
                 {
-                    hasDigits = true;
+                    if (char.IsDigit(symb))
+                    {
+                        hasDigits = true;
+                    }
+                    if (char.IsUpper(symb))
+                    {
+                        hasUpper = true;
+                    }
+                    if (char.IsLower(symb))
+                    {
+                        hasLower = true;
+                    }
                 }
-                if (char.IsUpper(symb))
+                if (!hasDigits)
                 {
-                    hasUpper = true;
+                    error.AppendLine("Минимум  1 цифра");
                 }
-                if (char.IsLower(symb))
+                if (!hasUpper)
                 {
-                    hasLower = true;
+                    error.AppendLine("Минимум 1 прописная буква");
                 }
-            }
-            if (!hasDigits)
-            {
-                error.AppendLine("Минимум  1 цифра");
-            }
-            if (!hasUpper)
-            {
-                error.AppendLine("Минимум 1 прописная буква");
-            }
-            if (!hasLower)
-            {
-                error.AppendLine("Минимум одна строчная буква");
-            }
-            if(TBPassword.Password != TBPasswordCheck.Password)
-            {
-                error.AppendLine("Пароли не совпадают");
-            }
-            if (DateTime.Now < DPAge.SelectedDate)
-            {
-                error.AppendLine("Дата выбранна некоректно");
-            }
-            else 
-            {
-                if ((DateTime.Now - DPAge.SelectedDate).Value.Total < 10)
+                if (!hasLower)
                 {
-                    error.AppendLine("Бегуну не может быть меньше 10 лет");
+                    error.AppendLine("Минимум одна строчная буква");
+                }
+                if (TBPassword.Password != TBPasswordCheck.Password)
+                {
+                    error.AppendLine("Пароли не совпадают");
+                }
+                if (DateTime.Now < DPAge.SelectedDate)
+                    {
+                        error.AppendLine("Дата выбранна некоректно");
+                    }
+                else
+                {
+                   if ((DateTime.Now - DPAge.SelectedDate).Value.TotalDays <= 3650)
+                    {
+                        error.AppendLine("Бегуну не может быть меньше 10 лет");
+                    }
+                }
+                Regex template = new Regex(@"^[A-z0-9.-]+@[A-z0-9]+\.[A-z0-9]+$");
+                if (!template.IsMatch(TBEmail.Text))
+                {
+                    error.AppendLine("Почта введена некоректно");
                 }
             }
-            Regex template = new Regex(@"^[A-z0-9.-]+@[A-z0-9]+\.[A-z0-9]+$");
-            if (template.IsMatch(TBEmail.Text)) return;
-            error.AppendLine("Почта введена некоректно");
             if (error.Length > 0)
             {
                 MessageBox.Show(error.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                 return;
             }
+            Entities.User currerntUser = new Entities.User
+            {
+                Email = TBEmail.Text,
+                Password = TBPassword.Password,
+                FirstName = TBFirstName.Text,
+                LastName = TBLastName.Text,
+                RoleId = "R"
+            };
+            AppData.Context.Users.Add(currerntUser);
+            Entities.Runner currentRunner = new Entities.Runner
+            {
+                Email = TBEmail.Text,
+                Gender = (CBGender.SelectedItem as Entities.Gender).Gender1,
+                DateOfBirth = DPAge.SelectedDate,
+                CountryCode = (CBCountry.SelectedItem as Entities.Country).CountryCode,
+                Photo = _imageData,
+                User = currerntUser
+            };
+            AppData.Context.Runners.Add(currentRunner);
+            AppData.Context.SaveChanges();
+            NavigationService.Navigate(new Page)
         }
 
         private void BtnChooseImage_Click(object sender, RoutedEventArgs e)
@@ -111,10 +138,8 @@ namespace MarathonApp.Pages.RunnerPage
             ofd.Filter = "Image formats | *.jpg; *.png";
             if(ofd.ShowDialog() == true)
             {
-                //byte[] imageData = null;
-                //  imageData = 
-                _filePath = ofd.FileName;
-                ImageProfile.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(_filePath);
+                _imageData = File.ReadAllBytes(ofd.FileName);
+                ImageProfile.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(_imageData);
             }
         }
     }
